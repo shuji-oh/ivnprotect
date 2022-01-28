e83549560742a587d9196294da22712c86e401ff
shuji-oh <ohira.shuji.ok2@is.naist.jp>
2022-01-28 13:15:17 +0900

    fixed script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo rmmod mcp251x_ivnprotect
-sudo insmod mcp251x_ivnprotect.ko
+sudo insmod mcp251x_ivnprotect/mcp251x_ivnprotect.ko
 sudo ip link set can0 type can bitrate 500000
 sudo ifconfig can0 txqueuelen 10000
 sudo ifconfig can0 down
